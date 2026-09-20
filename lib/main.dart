3ca8a3516fc9276e06aac11b274f97a02a6b44c0
thebest102022 <thebest102022@gmail.com>
2026-09-20 08:37:40 +0000

    feat: add Firebase Core foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'features/auth/medicare_entry_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MedicareApp());
 }
 
